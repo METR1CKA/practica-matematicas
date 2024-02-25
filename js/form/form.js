6d2849c02770051c4b918ab26b4cb0bf0d9f4762
METR1CKA <ferchosalazar054@gmail.com>
2024-02-25 06:31:57 -0600

    [UPD] v1.5.0
    - Euler mejorado refactorizado
    - Newton-Raphson refactorizado
    - Runge-Kutta de cuarto orden refactorizado
    - Validaciones

--- js/form/form.js
+++ js/form/form.js
@@ -6,6 +6,26 @@ const func_methods = {
   rungeKutta: rungeKuttaResult
 }
 
+const eq_error = {
+  eulerMejorado: 'em-f',
+  newtonRaphson: 'nr-f',
+  rungeKutta: 'rk4o-f'
+}
+
+function isValidEquation(equation) {
+  try {
+    let scope = { x: 0, y: 0 }
+
+    math.evaluate(equation, scope)
+  } catch (e) {
+    console.error({ error: e })
+
+    return false
+  }
+
+  return true
+}
+
 form.submit(event => {
   event.preventDefault()
 
@@ -20,6 +40,12 @@ form.submit(event => {
       obj[item.name] = item.value
       return obj
     }, {})
+
+  if (!isValidEquation(form_data[eq_error[method]])) {
+    return result.load('components/form/equation-error.html', function () {
+      window.scrollTo(0, document.body.scrollHeight)
+    })
+  }
 
   func_methods[method]({ decimales, form_data, result })
 })
